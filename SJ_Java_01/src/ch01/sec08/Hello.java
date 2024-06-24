0f77bbec5894a0e0017fb324a3b7df1b4f36c090
SungJin Park <pk942@naver.com>
2024-06-24 14:17:14 +0900

    my first commit 2024.06.24 I'm studying Java

---
+++ SJ_Java_01/src/ch01/sec08/Hello.java
@@ -0,0 +1,21 @@
+package ch01.sec08;
+
+/*
+ * 아아아아
+ * 2024년 6월 24일
+ * 강의 시작
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		//이것은 주석
+		System.out.println("sec08 입니다.");
+
+		int x;
+		x = 1;
+		int y = 2;
+		int result = x+y;
+		System.out.println(result);
+	}
+
+}
