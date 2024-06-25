e5ea4ff0cfa0235113b3a38d94d2310764da9f9b
SungJin Park <pk942@naver.com>
2024-06-25 12:43:20 +0900

    second day 2

---
+++ Ch02/src/Ch03/sec07/AndApp.java
@@ -0,0 +1,27 @@
+package Ch03.sec07;
+
+public class AndApp {
+
+	public static void main(String[] args) {
+		// ==============================
+				// # & &&
+				//	동일 기능
+				// ==============================
+				boolean b1 = true;
+				boolean b2 = false;
+
+				if (b1 & b2) {
+					System.out.println("b1 & b2=T");
+				} else {
+					System.out.println("b1 & b2=F");
+				}
+
+				if (b1 && b2) {
+					System.out.println("b1 && b2=T");
+				} else {
+					System.out.println("b1 && b2=F");
+				}
+
+	}
+
+}
