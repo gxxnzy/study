082283a1318722d58cf33b50a3e00fbf36ee1c8c
SungJin Park <pk942@naver.com>
2024-06-26 18:31:56 +0900

    Merge branch 'master' of https://github.com/jangyupeople/study.git

---
+++ Ch02/src/Ch06/sec07/exam01/Car.java
@@ -0,0 +1,7 @@
+package Ch06.sec07.exam01;
+
+public class Car {
+
+	Car(String model, String color, int maxSpeed){
+	}
+}
---
+++ Ch02/src/Ch06/sec07/exam01/CarExample.java
@@ -0,0 +1,9 @@
+package Ch06.sec07.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("그렌저","검정",250);
+	}
+
+}
